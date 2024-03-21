5408250bab3bff322d5386a2ebd1d7a1b256b21f
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-03-21 08:18:46 +0800

    changed disable to remove

--- 2_remove_console_access.py
+++ 2_remove_console_access.py
@@ -12,9 +12,9 @@ for user_name in user_names:
     try:
         # Remove the IAM user's login profile
         iam.delete_login_profile(UserName=user_name)
-        print(f"Console access disabled for user: {user_name}")
+        print(f"Console access removed for user: {user_name}")
     except ClientError as error:
         if error.response['Error']['Code'] == 'NoSuchEntity':
             print(f"The user {user_name} does not have a console login profile.")
         else:
-            print(f"Failed to disable console access for user: {user_name}. Error: {error}")
+            print(f"Failed to remove console access for user: {user_name}. Error: {error}")
