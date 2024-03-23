e116c9ad139d5ebdb000ff851366b9192f6034f3
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-03-23 15:20:29 +0800

    moved exclusions lowercase conversion to python script; script will now skip users with employeeID = service-account

--- 1_get_users_with_console_access.py
+++ 1_get_users_with_console_access.py
@@ -20,8 +20,8 @@ def parseArguments():
 
 def get_users_with_console_access(excluded_emails, excluded_usernames):
     # Initialize to empty lists regardless of input
-    excluded_emails = excluded_emails.split(',') if excluded_emails else []
-    excluded_usernames = excluded_usernames.split(',') if excluded_usernames else []
+    excluded_emails = excluded_emails.lower().split(',') if excluded_emails else []
+    excluded_usernames = excluded_usernames.lower().split(',') if excluded_usernames else []
     users_with_access = []  # This list will hold the usernames of users with console access
     
     # Assuming iam is a configured AWS IAM client
@@ -29,24 +29,27 @@ def get_users_with_console_access(excluded_emails, excluded_usernames):
     for response in paginator.paginate():
         for user in response['Users']:
             user_name = user['UserName']
+            
+            tags_response = iam.list_user_tags(UserName=user_name)
+            user_tags = {tag['Key']: tag['Value'] for tag in tags_response['Tags']} 
+            user_email = user_tags.get('email', None)  # Default to None if 'email' tag doesn't exist
+            employee_ID = user_tags.get('employeeID', None) # Default to None if 'employeeID' tag doesn't exist
+            
+            if employee_ID == 'service-account':
+                continue
+                
             try:
                 # Attempt to get the login profile for the user, indicating console access
                 iam.get_login_profile(UserName=user_name)
                 
-                # Login profile exists, now check if the user has an 'email' tag and if it's in the excluded list
-                tags_response = iam.list_user_tags(UserName=user_name)
-                user_tags = {tag['Key']: tag['Value'].lower() for tag in tags_response['Tags']}  # Convert tag values to lowercase
-                user_email = user_tags.get('email', None)  # Default to None if 'email' tag doesn't exist
-                
                 # Check if the user's email or username is in the excluded lists
-                if (not user_email or user_email not in excluded_emails) and user_name not in excluded_usernames:
+                if (not user_email or user_email.lower() not in excluded_emails) and user_name.lower() not in excluded_usernames:
                     users_with_access.append(user_name)
             except ClientError as error:
                 # Log the exception and move on
                 pass
 
     return ','.join(users_with_access)
-
 
 # Parse the arguments
 args = parseArguments()
